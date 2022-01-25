a23f41903d094517e671de2bf6abe744a67c15d9
bsspencer42 <mk8golfr2022@gmail.com>
2022-01-25 12:29:18 -0500

    Check if SSH works.

--- src/mk8golfr2022.java
+++ src/mk8golfr2022.java
@@ -1,5 +1,8 @@
+import java.sql.SQLOutput;
+
 public class mk8golfr2022 {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("After SSH key");
     }
 }
